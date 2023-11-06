platformengineerid/grafana-mimir

add client for active series

pkg/querier/cardinality_analysis_handler.go
@@ -10,6 +10,7 @@ import (
 	"github.com/grafana/dskit/httpgrpc"
 	"github.com/grafana/dskit/tenant"
 	"github.com/pkg/errors"
+	"github.com/prometheus/prometheus/model/labels"
 
 	"github.com/grafana/mimir/pkg/cardinality"
 	ingester_client "github.com/grafana/mimir/pkg/ingester/client"
@@ -75,6 +76,38 @@ func LabelValuesCardinalityHandler(distributor Distributor, limits *validation.O
 		}
 
 		util.WriteJSONResponse(w, toLabelValuesCardinalityResponse(seriesCountTotal, cardinalityResponse, cardinalityRequest.Limit))
+	})
+}
+
+func ActiveSeriesCardinalityHandler(distributor Distributor, limits *validation.Overrides) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		// Guarantee request's context is for a single tenant id
+		tenantID, err := tenant.TenantID(ctx)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if !limits.CardinalityAnalysisEnabled(tenantID) {
+			http.Error(w, fmt.Sprintf("cardinality analysis is disabled for the tenant: %v", tenantID), http.StatusBadRequest)
+			return
+		}
+
+		matchers, err := cardinality.DecodeActiveSeriesRequest(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		res, err := distributor.ActiveSeries(ctx, matchers)
+		if err != nil {
+			respondFromError(err, w)
+			return
+		}
+
+		// TODO convert to the right format
+		util.WriteJSONResponse(w, ActiveSeriesResponse{res})
 	})
 }
 
@@ -204,4 +237,8 @@ type labelNamesCardinality struct {
 type labelValuesCardinalityResponse struct {
 	SeriesCountTotal uint64                  `json:"series_count_total"`
 	Labels           []labelNamesCardinality `json:"labels"`
+}
+
+type ActiveSeriesResponse struct {
+	Data []labels.Labels `json:"data"`
 }

fyang13/mimir

pkg/querier/cardinality_analysis_handler.go
@@ -10,6 +10,7 @@ import (
 	"github.com/grafana/dskit/httpgrpc"
 	"github.com/grafana/dskit/tenant"
 	"github.com/pkg/errors"
+	"github.com/prometheus/prometheus/model/labels"
 
 	"github.com/grafana/mimir/pkg/cardinality"
 	ingester_client "github.com/grafana/mimir/pkg/ingester/client"
@@ -75,6 +76,38 @@ func LabelValuesCardinalityHandler(distributor Distributor, limits *validation.O
 		}
 
 		util.WriteJSONResponse(w, toLabelValuesCardinalityResponse(seriesCountTotal, cardinalityResponse, cardinalityRequest.Limit))
+	})
+}
+
+func ActiveSeriesCardinalityHandler(distributor Distributor, limits *validation.Overrides) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		// Guarantee request's context is for a single tenant id
+		tenantID, err := tenant.TenantID(ctx)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if !limits.CardinalityAnalysisEnabled(tenantID) {
+			http.Error(w, fmt.Sprintf("cardinality analysis is disabled for the tenant: %v", tenantID), http.StatusBadRequest)
+			return
+		}
+
+		req, err := cardinality.DecodeActiveSeriesRequest(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		res, err := distributor.ActiveSeries(ctx, req.MatcherSet)
+		if err != nil {
+			respondFromError(err, w)
+			return
+		}
+
+		// TODO convert to the right format
+		util.WriteJSONResponse(w, activeSeriesResponse{res})
 	})
 }
 
@@ -204,4 +237,8 @@ type labelNamesCardinality struct {
 type labelValuesCardinalityResponse struct {
 	SeriesCountTotal uint64                  `json:"series_count_total"`
 	Labels           []labelNamesCardinality `json:"labels"`
+}
+
+type activeSeriesResponse struct {
+	Data []labels.Labels `json:"data"`
 }